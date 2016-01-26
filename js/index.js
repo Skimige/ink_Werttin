dc2430d4ec92b8c6f74c3e80c9c59f7cab0de900
Skimige <Skimige@users.noreply.github.com>
2016-01-26 20:57:10 +0800

    Changes from Issue #1

--- js/index.js
+++ js/index.js
@@ -5,23 +5,23 @@
     var interval, seconds;
     seconds = Math.floor((new Date() - date) / 1000);
     interval = Math.floor(seconds / 31536000);
-    if (interval > 1) {
+    if (interval >= 1) {
       return interval + " years ago";
     }
     interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
+    if (interval >= 1) {
       return interval + " months ago";
     }
     interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
+    if (interval >= 1) {
       return interval + " days ago";
     }
     interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
+    if (interval >= 1) {
       return interval + " hours ago";
     }
     interval = Math.floor(seconds / 60);
-    if (interval > 1) {
+    if (interval >= 1) {
       return interval + " mins ago";
     }
     return Math.floor(seconds) + " seconds ago";
